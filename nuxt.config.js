f448b313f45d12dd4f46bc7e23197e0fc1fdddb8
Ridhan Sholeh <ridhannewbiecheaters@gmail.com>
2021-06-10 11:14:32 +0700

    Konversi Halaman Html to Vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,9 @@ export default {
 				href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap',
 			},
 		],
+		bodyAttrs: {
+			class : 'antialiased'
+		}
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
